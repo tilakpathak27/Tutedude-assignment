35b6a14daf1a889fdd94aa9c180f4100c71842f5
Tilak pathak <tilakpathak712@gnail.com>
2025-10-28 09:58:28 +0530

    Initial commit

---
+++ assigmnet-1.py
@@ -0,0 +1,15 @@
+# Task=1
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("Addition:",add)
+print("Subtraction:",sub)
+print("Multiplication",mul)
+print("Division:",div)
+
+
+
+# task2
