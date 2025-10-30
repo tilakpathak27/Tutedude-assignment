e3df9dca43449f0ce6d8082a784b69409ef8e1f5
Tilak Pathak <tilakpathak712@gmail.com>
2025-10-30 12:31:08 +0530

    Update assignment-2.py

--- assignment-2.py
+++ assignment-2.py
@@ -1,5 +1,13 @@
+# task-1
 a=int(input("Enter a number:"))
 if a%2==0:
     print(a,"is an even number")
 else:
     print(a,"is an odd number")
+
+
+# task-2
+k=0
+for i in range(0,51):
+   k=i+k
+print("The sum of number from 1 to 50 is:",k)
