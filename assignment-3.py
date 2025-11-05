65d833d72e5a4db9309901fb753a12d823f16e6d
Tilak Pathak <tilakpathak712@gmail.com>
2025-11-05 09:35:27 +0530

    Tutedude assigment no 03

--- assignment-3.py
+++ assignment-3.py
@@ -1,3 +1,4 @@
+# Task-1
 def factorial(n):
     result = 1
     for i in range(1, n + 1):
@@ -6,3 +7,15 @@ def factorial(n):
 
 number = int(input("Enter the number: "))
 print("The factorial of", number, "is:", factorial(number))
+
+
+
+# task-2
+import math
+num = float(input("Enter a number: "))
+sqrt_val = math.sqrt(num)
+log_val = math.log(num)
+sin_val = math.sin(num)
+print("Square root of", num, "is:", sqrt_val)
+print("Natural log of", num, "is:", log_val)
+print("Sine of", num, "is:", sin_val)
